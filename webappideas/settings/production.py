674811ebfad79d786bcd7777a8d594ea56bfcb34
Sean <29239704+Bakersbakebread@users.noreply.github.com>
2020-03-11 12:13:11 +0000

    Update production.py

--- webappideas/settings/production.py
+++ webappideas/settings/production.py
@@ -3,6 +3,6 @@ from webappideas.settings.common import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["localhost"]
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS")
 
 SECRET_KEY = os.getenv("SECRET_KEY")
